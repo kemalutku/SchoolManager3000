2ae4122483aa26c712c56a3ece3ed3dfcdd83beb
Utku <kemalutkulekesiz@gmail.com>
2023-10-30 21:19:25 +0300

    Added online db connection

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import mysql.connector
 import tkinter as tk
 
+
+
+mydb = mysql.connector.connect(
+    host="5.75.140.90",
+    user="bil071",
+    password='Bil071-24'
+)
+
+cursor = mydb.cursor()
+cursor.execute("SHOW DATABASES")
+
+
+
 root = tk.Tk()
-label = tk.Label(root, text="Hello World")
-label.pack()
+for x in cursor:
+    label = tk.Label(root, text=str(x))
+    label.pack()
 root.mainloop()
