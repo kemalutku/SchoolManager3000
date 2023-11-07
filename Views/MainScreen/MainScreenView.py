a4a09d8bcbb92390ad8422c281654daec9b96a77
Utku Lekesiz <kemalutkulekesiz@gmail.com>
2023-11-08 00:02:15 +0300

    Fixed to ManagementView buttons disappering

--- Views/MainScreen/MainScreenView.py
+++ Views/MainScreen/MainScreenView.py
@@ -8,10 +8,11 @@ class MainScreenView(tk.Frame):
         self.frame_name = "MainScreenView"
         self.cont = cont
 
-        logout_frame =tk.Frame(self)
+        logout_frame = tk.Frame(self)
         name_label = tk.Label(logout_frame, text="Okul Yönetim Sistemi 3000")
         name_label.pack(side="left")
-        logout_button = tk.Button(logout_frame, text="Çıkış Yap", command=lambda :cont.show_frame(Views.LoginScreenView))
+        logout_button = tk.Button(logout_frame, text="Çıkış Yap",
+                                  command=lambda: cont.show_frame(Views.LoginScreenView))
         logout_button.config(bg="red")
         logout_button.pack(side="right")
         logout_frame.pack(side="top", fill="x")
@@ -24,24 +25,30 @@ class MainScreenView(tk.Frame):
         buttons_frame.grid_columnconfigure(0, weight=1)
         buttons_frame.grid_columnconfigure(1, weight=1)
 
-        student_button = tk.Button(buttons_frame, text="Öğrenci Yönetimi", command=lambda: self.open_management_view(Views.STUDENT))
-        employee_button = tk.Button(buttons_frame, text="Çalışan Yönetimi", command=lambda: self.open_management_view(Views.EMPLOYEE))
-        course_button = tk.Button(buttons_frame, text="Ders Yönetimi", command=lambda: cont.show_frame(None))
-        accounting_button = tk.Button(buttons_frame, text="Muhasebe", command=lambda: cont.show_frame(None))
-        stock_button = tk.Button(buttons_frame, text="Stok Takibi", command=lambda: cont.show_frame(None))
-        contact_button = tk.Button(buttons_frame, text="İletişim", command=lambda: cont.show_frame(None))
+        student_button = tk.Button(buttons_frame, text="Öğrenci Yönetimi",
+                                   command=lambda: self.open_management_view(Views.STUDENT))
+        employee_button = tk.Button(buttons_frame, text="Çalışan Yönetimi",
+                                    command=lambda: self.open_management_view(Views.EMPLOYEE))
+        course_button = tk.Button(buttons_frame, text="Ders Yönetimi",
+                                  command=lambda: self.open_management_view(Views.ACTIVITY))
+        accounting_button = tk.Button(buttons_frame, text="Muhasebe",
+                                      command=lambda: cont.show_frame(None))
+        stock_button = tk.Button(buttons_frame, text="Stok Takibi",
+                                 command=lambda: cont.show_frame(None))
+        contact_button = tk.Button(buttons_frame, text="İletişim",
+                                   command=lambda: cont.show_frame(None))
 
-        student_button.grid(row=0, column=0, sticky="nsew", padx=[10,100], pady=20)
-        employee_button.grid(row=1, column=0, sticky="nsew", padx=[10,100], pady=20)
-        course_button.grid(row=2, column=0, sticky="nsew", padx=[10,100], pady=20)
-        accounting_button.grid(row=0, column=1, sticky="nsew", padx=[100,10], pady=20)
-        stock_button.grid(row=1, column=1, sticky="nsew", padx=[100,10], pady=20)
-        contact_button.grid(row=2, column=1, sticky="nsew", padx=[100,10], pady=20)
+        student_button.grid(row=0, column=0, sticky="nsew", padx=[10, 100], pady=20)
+        employee_button.grid(row=1, column=0, sticky="nsew", padx=[10, 100], pady=20)
+        course_button.grid(row=2, column=0, sticky="nsew", padx=[10, 100], pady=20)
+        accounting_button.grid(row=0, column=1, sticky="nsew", padx=[100, 10], pady=20)
+        stock_button.grid(row=1, column=1, sticky="nsew", padx=[100, 10], pady=20)
+        contact_button.grid(row=2, column=1, sticky="nsew", padx=[100, 10], pady=20)
 
         buttons_frame.pack(side="top", expand=True, fill="both")
 
         logo_frame = tk.Frame(self)
-        logo_frame.pack(side="top",expand=True, fill="both")
+        logo_frame.pack(side="top", expand=True, fill="both")
 
     def open_management_view(self, mode):
         management_view = self.cont.get_frame(Views.SummaryView)
--- Views/ManagementView/SummaryView.py
+++ Views/ManagementView/SummaryView.py
@@ -18,30 +18,87 @@ class SummaryView(tk.Frame):
         title_label = tk.Label(title_frame, textvariable=self.title)
         back_button.pack(side="left")
         title_label.pack(side="left", fill="x")
-        title_frame.pack(side="top", fill="x")
+        title_frame.pack(side="top", fill="x", pady=5, padx=5)
 
         summary_frame = tk.Frame(self)
+        summary_frame.columnconfigure(0, weight=1)
+        summary_frame.rowconfigure(0, weight=1)
         self.tree_view = ttk.Treeview(summary_frame)
-        self.tree_view.pack(side="left", expand=True, fill="both")
+        self.tree_view.grid(row=0, column=0, sticky="nsew")
 
         button_frame = tk.Frame(summary_frame)
-        add_new_button = tk.Button(button_frame, text="Ekle")
-        syllabus_button = tk.Button(button_frame, text="Ders Programını\nGöster")
-        add_relation_button = tk.Button(button_frame, text="Ders Ekle/Sil")
-        show_details_button = tk.Button(button_frame, text="Bilgileri Göster")
+        self.add_new_button = tk.Button(button_frame, text="Ekle", height=5)
+        syllabus_button = tk.Button(button_frame, text="Ders Programını\nGöster", height=5)
+        add_relation_button = tk.Button(button_frame, text="Ders Ekle / Sil", height=5)
+        show_details_button = tk.Button(button_frame, text="Bilgileri Göster", height=5)
 
-        add_new_button.pack(side="top", fill="x")
-        syllabus_button.pack(side="top", fill="x")
-        add_relation_button.pack(side="top", fill="x")
-        show_details_button.pack(side="top", fill="x")
-        button_frame.pack(side="left", fill="both")
+        self.add_new_button.pack(side="top", fill="x", pady=20, padx=10)
+        syllabus_button.pack(side="top", fill="x", pady=20, padx=10)
+        add_relation_button.pack(side="top", fill="x", pady=20, padx=10)
+        show_details_button.pack(side="top", fill="x", pady=20, padx=10)
 
-        summary_frame.pack(side="top", expand=True, fill="both")
+        button_frame.grid(row=0, column=1)
+
+        summary_frame.pack(side="top", expand=True, fill="both", padx=10, pady=10)
 
     def set_mode(self, mode):
         if mode == STUDENT:
             self.title.set("Öğrenciler")
+            self.tree_view['columns'] = ('okul_num', 'ad', 'soyad', 'yas', 'veli_ad', 'veli_soyad')
+
+            self.tree_view.column('#0', width=0, stretch=tk.NO)
+            self.tree_view.column("okul_num", width=150)
+            self.tree_view.column("ad", width=150)
+            self.tree_view.column("soyad", width=150)
+            self.tree_view.column("yas", width=150)
+            self.tree_view.column("veli_ad", width=150)
+            self.tree_view.column("veli_soyad", width=150)
+
+            self.tree_view.heading("okul_num", text="Okul Numarası")
+            self.tree_view.heading("ad", text="Ad")
+            self.tree_view.heading("soyad", text="Soyad")
+            self.tree_view.heading("yas", text="Yaş")
+            self.tree_view.heading("veli_ad", text="Veli Adı")
+            self.tree_view.heading("veli_soyad", text="Veli Soyadı")
+
+            self.add_new_button.config(text="Öğrenci Ekle")
+
         elif mode == EMPLOYEE:
             self.title.set("Çalışanlar")
+            self.tree_view['columns'] = ('okul_num', 'ad', 'soyad', 'yas', 'e-mail', 'maas')
+
+            self.tree_view.column('#0', width=0, stretch=tk.NO)
+            self.tree_view.column("okul_num", width=150)
+            self.tree_view.column("ad", width=150)
+            self.tree_view.column("soyad", width=150)
+            self.tree_view.column("yas", width=150)
+            self.tree_view.column("e-mail", width=150)
+            self.tree_view.column("maas", width=150)
+
+            self.tree_view.heading("okul_num", text="Çalışan Numarası", anchor=tk.W)
+            self.tree_view.heading("ad", text="Ad", anchor=tk.W)
+            self.tree_view.heading("soyad", text="Soyad", anchor=tk.W)
+            self.tree_view.heading("yas", text="Yaş", anchor=tk.W)
+            self.tree_view.heading("e-mail", text="E-Mail", anchor=tk.W)
+            self.tree_view.heading("maas", text="Maaş", anchor=tk.W)
+
+            self.add_new_button.config(text="Çalışan Ekle")
+
         elif mode == ACTIVITY:
             self.title.set("Dersler")
+            self.tree_view['columns'] = ('ders_num', 'ad', 'gun', 'b_saati', 'type')
+
+            self.tree_view.column('#0', width=0, stretch=tk.NO)
+            self.tree_view.column("ders_num", width=150)
+            self.tree_view.column("ad", width=150)
+            self.tree_view.column("gun", width=150)
+            self.tree_view.column("b_saati", width=150)
+            self.tree_view.column("type", width=150)
+
+            self.tree_view.heading("ders_num", text="Ders Numarası", anchor=tk.W)
+            self.tree_view.heading("ad", text="Ad", anchor=tk.W)
+            self.tree_view.heading("gun", text="Gün", anchor=tk.W)
+            self.tree_view.heading("b_saati", text="Başlangıç Saati", anchor=tk.W)
+            self.tree_view.heading("type", text="Tür", anchor=tk.W)
+
+            self.add_new_button.config(text="Çalışan Ekle")
